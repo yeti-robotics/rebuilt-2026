1940e41594ec902e0c5e07fd16146768e06d24dc
Taran Duba <taran.duba3@gmail.com>
2026-01-13 19:07:55 -0500

    removed unnecessary template vendordeps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "rebuilt-2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 6;
-    public static final String GIT_SHA = "9e5d699778f84498a3e2c86c1126d2bd5086adc8";
-    public static final String GIT_DATE = "2026-01-11 09:56:07 EST";
+    public static final int GIT_REVISION = 7;
+    public static final String GIT_SHA = "a16cbbea0a2d390a97ce0863c533816e36a9a0f9";
+    public static final String GIT_DATE = "2026-01-13 19:04:41 EST";
     public static final String GIT_BRANCH = "akitting";
-    public static final String BUILD_DATE = "2026-01-13 19:04:18 EST";
-    public static final long BUILD_UNIX_TIME = 1768349058307L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2026-01-13 19:07:37 EST";
+    public static final long BUILD_UNIX_TIME = 1768349257496L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
