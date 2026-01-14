bb6104e925eede40051b62b14c3952d0a0184b4c
Taran Duba <taran.duba3@gmail.com>
2026-01-13 20:34:54 -0500

    added transition patterns to be used

--- src/main/java/frc/robot/subsystems/led/LEDModes.java
+++ src/main/java/frc/robot/subsystems/led/LEDModes.java
@@ -4,9 +4,10 @@ import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 
 public enum LEDModes {
-    RED_ALLIANCE_ACTIVE(LEDPattern.solid(Color.kRed)),
+    RED_ALLIANCE_ACTIVE(LEDPattern.solid(new Color(197, 34, 51))),
+    RED_TO_BLUE_TRANSITION(LEDPattern.gradient(LEDPattern.GradientType.kContinuous, new Color(197, 34, 51), new Color(84, 182, 229))),
+    BLUE_TO_RED_TRANSITION(LEDPattern.gradient(LEDPattern.GradientType.kContinuous, new Color(84, 182, 229), new Color(197, 34, 51))),
     BLUE_ALLIANCE_ACTIVE(LEDPattern.solid(new Color(84, 182, 229)));
-
     public final LEDPattern pattern;
 
     LEDModes(LEDPattern pattern) {
