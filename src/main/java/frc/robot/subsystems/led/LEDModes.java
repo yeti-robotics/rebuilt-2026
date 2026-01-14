539345928e543dc1709c6ea2ac3786799cfc112b
Taran Duba <taran.duba3@gmail.com>
2026-01-13 22:22:44 -0500

    a lil bit of "abstraction" as my csp teacher would say

--- src/main/java/frc/robot/subsystems/led/LEDModes.java
+++ src/main/java/frc/robot/subsystems/led/LEDModes.java
@@ -5,9 +5,9 @@ import edu.wpi.first.wpilibj.util.Color;
 
 public enum LEDModes {
     RED_ALLIANCE_ACTIVE(LEDPattern.solid(new Color(197, 34, 51))),
-    RED_TO_BLUE_TRANSITION(LEDPattern.gradient(LEDPattern.GradientType.kContinuous, new Color(197, 34, 51), new Color(84, 182, 229))),
-    BLUE_TO_RED_TRANSITION(LEDPattern.gradient(LEDPattern.GradientType.kContinuous, new Color(84, 182, 229), new Color(197, 34, 51))),
-    BLUE_ALLIANCE_ACTIVE(LEDPattern.solid(new Color(84, 182, 229)));
+    RED_TO_BLUE_TRANSITION(LEDPattern.gradient(LEDPattern.GradientType.kContinuous, LEDConstants.VIHAAN_RED, LEDConstants.YETI_BLUE)),
+    BLUE_TO_RED_TRANSITION(LEDPattern.gradient(LEDPattern.GradientType.kContinuous, LEDConstants.YETI_BLUE, LEDConstants.VIHAAN_RED)),
+    BLUE_ALLIANCE_ACTIVE(LEDPattern.solid(LEDConstants.YETI_BLUE));
     public final LEDPattern pattern;
 
     LEDModes(LEDPattern pattern) {
