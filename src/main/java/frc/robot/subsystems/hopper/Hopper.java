61c85235a988791c60324992eaf2289ea6ad4813
amitnchoudhary <amazingamitty@gmail.com>
2026-01-16 19:35:37 -0500

    added logger.processinputs

--- src/main/java/frc/robot/subsystems/hopper/Hopper.java
+++ src/main/java/frc/robot/subsystems/hopper/Hopper.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.hopper;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
 
 public class Hopper extends SubsystemBase {
 
@@ -11,6 +12,7 @@ public class Hopper extends SubsystemBase {
     @Override
     public void periodic() {
         io.updateInputs(inputs);
+        Logger.processInputs("Hopper", inputs);
     }
 
     public Hopper(HopperIO io) {
