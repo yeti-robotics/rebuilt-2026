e3671c630240e529d0f385217f9aa4f96267dace
qwertycloudhub <vihaansden@gmail.com>
2026-01-17 03:13:12 -0500

    sins of the coder sins of the code

--- src/main/java/frc/robot/commands/AutoAim.java
+++ src/main/java/frc/robot/commands/AutoAim.java
@@ -1,53 +1,62 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.constants.*;
 import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.vision.*;
 import frc.robot.util.AllianceFlipUtil;
-import frc.robot.util.LimelightHelpers;
+import frc.robot.constants.FieldConstants;
 import java.util.function.DoubleSupplier;
 
 public class AutoAim extends Command {
 
     private final Drive drivetrain;
     private final DoubleSupplier xVelSupplier;
-    private final TurnToPoint poseAimRequest;
     private final DoubleSupplier yVelSupplier;
+    private final TurnToPoint turnToPoint;
     private double currentTag;
 
     public AutoAim(Drive drivetrain, DoubleSupplier xVelSupplier, DoubleSupplier yVelSupplier) {
-
         this.drivetrain = drivetrain;
         this.xVelSupplier = xVelSupplier;
         this.yVelSupplier = yVelSupplier;
 
-        addRequirements(drivetrain);
+        turnToPoint = new TurnToPoint();
 
-        poseAimRequest = new TurnToPoint();
-        poseAimRequest.HeadingController.setPID(5, 0, 0);
-        poseAimRequest.HeadingController.enableContinuousInput(-Math.PI, Math.PI);
+        addRequirements(drivetrain);
     }
 
     @Override
     public void initialize() {
-        currentTag = LimelightHelpers.getFiducialID(VisionConstants.camera0Name);
 
         Translation2d speakerCenter = AllianceFlipUtil.apply(FieldConstants.Hub.centerHubOpening.toTranslation2d());
-
-        poseAimRequest.setPointToFace(speakerCenter);
+        turnToPoint.setPointToFace(speakerCenter);
     }
 
     @Override
     public void execute() {
-        drivetrain.run(() -> poseAimRequest
-                .withVelocityX(xVelSupplier.getAsDouble() * 1.5)
-                .withVelocityY(yVelSupplier.getAsDouble() * 1.5));
+        Pose2d currentPose = drivetrain.getPose();
+
+        double omega = turnToPoint.calculateAngularVelocity(currentPose);
+
+        drivetrain.runVelocity(
+                ChassisSpeeds.fromFieldRelativeSpeeds(
+                        xVelSupplier.getAsDouble() * 1.5,
+                        yVelSupplier.getAsDouble() * 1.5,
+                        omega,
+                        drivetrain.getRotation()
+                )
+        );
     }
 
     @Override
     public boolean isFinished() {
         return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.runVelocity(new ChassisSpeeds());
     }
 }
