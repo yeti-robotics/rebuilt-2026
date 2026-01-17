1acc4ad934ac697509057e12eee67ac4f7c1421b
qwertycloudhub <vihaansden@gmail.com>
2026-01-16 23:20:11 -0500

    Auto align dimensions completed
    (i may not know, where im heading in life but at least the robot knows its heading)

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -7,13 +7,15 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
 public class FieldConstants {
-    public static final double FIELD_LENGTH = 0;
+    public static final double FIELD_LENGTH = Units.inchesToMeters(651.2);
+    public static final double FIELD_WIDTH = 317.7; //inches
 
     public static final class Hub {
+        public static final double CENTER_HUB_X = 158.6; //inches
         public static Translation3d farLeftHub =
-                new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0));
+                new Translation3d(Units.inchesToMeters((CENTER_HUB_X + 47)/2), Units.inchesToMeters((FIELD_WIDTH + 47)/2), Units.inchesToMeters(0));
         public static Translation3d closeRightHub =
-                new Translation3d(0.0, Units.inchesToMeters(0), Units.inchesToMeters(0));
+                new Translation3d(Units.inchesToMeters((CENTER_HUB_X - 47)/2), Units.inchesToMeters((FIELD_WIDTH - 47)/2), Units.inchesToMeters(0));
         public static Translation3d centerHubOpening = farLeftHub.interpolate(closeRightHub, 0.5);
         public static final AprilTagFieldLayout aprilTags;
 
