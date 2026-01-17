3707b9d7a1447cdf7f61197200eb798d4cb672c4
John Doe <johndoe@email.com>
2026-01-17 15:31:48 -0500

    autonamedcommands fixed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "rebuilt-2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 64;
-    public static final String GIT_SHA = "e15dda1d03cde941f5950844caab48a7273bfbac";
-    public static final String GIT_DATE = "2026-01-17 11:48:47 EST";
-    public static final String GIT_BRANCH = "climber";
-    public static final String BUILD_DATE = "2026-01-17 14:41:46 EST";
-    public static final long BUILD_UNIX_TIME = 1768678906231L;
-    public static final int DIRTY = 1;
+    public static final int GIT_REVISION = 99;
+    public static final String GIT_SHA = "6f443a4c46a357ff971ad29e12566d5dc6b32551";
+    public static final String GIT_DATE = "2026-01-17 15:26:46 EST";
+    public static final String GIT_BRANCH = "pathplanner";
+    public static final String BUILD_DATE = "2026-01-17 15:28:23 EST";
+    public static final long BUILD_UNIX_TIME = 1768681703389L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
