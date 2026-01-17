db3a87b5ba832db21eb424849eca39a34182d99b
amitnchoudhary <amazingamitty@gmail.com>
2026-01-17 16:19:08 -0500

    changed motor and concxoader id

--- src/main/java/frc/robot/subsystems/linslide/LinSlideConfigsAlpha.java
+++ src/main/java/frc/robot/subsystems/linslide/LinSlideConfigsAlpha.java
@@ -8,8 +8,8 @@ import com.ctre.phoenix6.signals.SensorDirectionValue;
 import frc.robot.Robot;
 
 public class LinSlideConfigsAlpha {
-    static final int LIN_SLIDE_MOTOR_ID = 99;
-    static final int LIN_SLIDE_CANCODER_ID = 98;
+    static final int LIN_SLIDE_MOTOR_ID = 60;
+    static final int LIN_SLIDE_CANCODER_ID = 61;
     static final double GEAR_RATIO = 3;
 
     private static final Slot0Configs SLOT_0_CONFIGS = Robot.isReal()
