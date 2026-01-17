b1a53fb2be0d5cbe4528e46a6d32ded1ff9c7071
qwertycloudhub <vihaansden@gmail.com>
2026-01-16 20:59:33 -0500

    Auto Aim
    (I SAY AUTO, YOU SAY ALIGN)

---
+++ src/main/java/frc/robot/commands/AutoAim.java
@@ -0,0 +1,62 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.*;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.vision.*;
+import frc.robot.util.AllianceFlipUtil;
+import frc.robot.util.LimelightHelpers;
+import java.util.function.DoubleSupplier;
+
+public class AutoAim extends Command {
+
+    private final Drive drivetrain;
+    private final DoubleSupplier xVelSupplier;
+    private final TurnToPoint poseAimRequest;
+    private final DoubleSupplier yVelSupplier;
+    private double currentTag;
+
+    public AutoAim(Drive drivetrain, DoubleSupplier xVelSupplier, DoubleSupplier yVelSupplier) {
+
+        this.drivetrain = drivetrain;
+        this.xVelSupplier = xVelSupplier;
+        this.yVelSupplier = yVelSupplier;
+
+        addRequirements(drivetrain);
+
+        poseAimRequest = new TurnToPoint();
+        poseAimRequest.HeadingController.setPID(5, 0, 0);
+        poseAimRequest.HeadingController.enableContinuousInput(-Math.PI, Math.PI);
+    }
+
+    @Override
+    public void initialize() {
+        currentTag = LimelightHelpers.getFiducialID(
+                VisionConstants.camera0Name.isBlank() ? VisionConstants.camera0Name : VisionConstants.camera1Name);
+
+        Translation2d speakerCenter = AllianceFlipUtil.apply(FieldConstants.Hub.centerHubOpening.toTranslation2d());
+
+        poseAimRequest.setPointToFace(speakerCenter);
+    }
+
+    @Override
+    public void execute() {
+        if (LimelightHelpers.getFiducialID(
+                        VisionConstants.camera0Name.isBlank()
+                                ? VisionConstants.camera0Name
+                                : VisionConstants.camera1Name)
+                == currentTag) {
+            drivetrain.run(() -> poseAimRequest
+                    .withVelocityX(xVelSupplier.getAsDouble() * 1.5)
+                    .withVelocityY(yVelSupplier.getAsDouble() * 1.5));
+        } else {
+            end(true);
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -1,5 +1,33 @@
 package frc.robot.constants;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+
 public class FieldConstants {
     public static final double FIELD_LENGTH = 0;
+
+    public static final class Hub {
+        public static Translation3d farLeftHub =
+                new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0));
+        public static Translation3d closeRightHub =
+                new Translation3d(0.0, Units.inchesToMeters(0), Units.inchesToMeters(0));
+        public static Translation3d centerHubOpening = farLeftHub.interpolate(closeRightHub, 0.5);
+        public static final AprilTagFieldLayout aprilTags;
+
+        static {
+            aprilTags = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltAndymark);
+        }
+    }
+
+    public static final class Shuttle {
+        public static Translation2d closeShuttleTargetCorner = new Translation2d(0.0, 0.0);
+
+        public static Translation2d farShuttleTargetCorner = new Translation2d(0.0, 0.0);
+
+        public static Translation2d shuttleTargetZone =
+                closeShuttleTargetCorner.interpolate(farShuttleTargetCorner, 0.5);
+    }
 }
