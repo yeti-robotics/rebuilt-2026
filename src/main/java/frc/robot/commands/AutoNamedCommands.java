aada9bff16475edf74861e2a1fc41818b664fa8c
John Doe <johndoe@email.com>
2026-01-17 15:34:18 -0500

    climber to autonamedcommands

--- src/main/java/frc/robot/commands/AutoNamedCommands.java
+++ src/main/java/frc/robot/commands/AutoNamedCommands.java
@@ -4,6 +4,7 @@ import static com.pathplanner.lib.auto.NamedCommands.registerCommands;
 
 import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.climber.Climber;
 import frc.robot.subsystems.hopper.Hopper;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
@@ -12,20 +13,20 @@ public class AutoNamedCommands {
     private final IntakeSubsystem intake;
     private final Hopper hopper;
     private final ShooterSubsystem shooter;
+    private final Climber climber;
 
-    public AutoNamedCommands(IntakeSubsystem intake, Hopper hopper, ShooterSubsystem shooter) {
+    public AutoNamedCommands(IntakeSubsystem intake, Hopper hopper, ShooterSubsystem shooter, Climber climber) {
         this.intake = intake;
         this.hopper = hopper;
         this.shooter = shooter;
+        this.climber = climber;
         registerCommands();
     }
 
     public void registerCommands() {
-        NamedCommands.registerCommand("GroundIntake", Commands.sequence(
-                hopper.spinHopper(0.0),
+        NamedCommands.registerCommand("GroundIntake", Commands.sequence(hopper.spinHopper(0.0),
                 intake.setRoller(0.0)));
-        NamedCommands.registerCommand("Shoot", Commands.sequence(
-                hopper.spinHopper(0),
+        NamedCommands.registerCommand("Shoot", Commands.sequence(hopper.spinHopper(0),
                 shooter.shoot(0)));
     }
 }
