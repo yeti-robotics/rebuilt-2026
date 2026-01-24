f1a7c01e208e2428bacb6b820490360f81c05922
Prathik Saduneni <151557515+prthik@users.noreply.github.com>
2026-01-24 15:15:45 -0500

    motion magic voltage not position duty cycle out...my bad

--- src/main/java/frc/robot/subsystems/hood/HoodIOBeta.java
+++ src/main/java/frc/robot/subsystems/hood/HoodIOBeta.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.hood;
 
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -10,7 +11,7 @@ import frc.robot.util.sim.PhysicsSim;
 public class HoodIOBeta implements HoodIO {
     private final TalonFX hoodMotor;
     private final CANcoder hoodCANcoder;
-    private final PositionDutyCycle positionRequest = new PositionDutyCycle(0.0);
+    private final MotionMagicVoltage positionRequest = new MotionMagicVoltage(0.0);
 
     public HoodIOBeta() {
         hoodMotor = new TalonFX(HoodConfigs.HOOD_MOTOR_ID, Constants.rioBus);
