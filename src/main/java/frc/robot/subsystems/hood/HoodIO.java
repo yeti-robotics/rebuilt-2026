85f536bc4edf13e4ee08e1857ae015270d3b1e79
Prathik Saduneni <151557515+prthik@users.noreply.github.com>
2026-01-24 10:36:40 -0500

    simple interface

---
+++ src/main/java/frc/robot/subsystems/hood/HoodIO.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.hood;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface HoodIO {
+    @AutoLog
+    class HoodIOInputs {
+        public double hoodPosition = 0.0;
+        public double hoodVelocity = 0.0;
+    }
+
+    default void updateInputs(HoodIOInputs inputs) {
+    }
+
+    default void setPosition(double position) {
+    }
+}
