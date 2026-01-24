f4b85158f3ce18bbd117931278a77ad470d27fca
Prathik Saduneni <151557515+prthik@users.noreply.github.com>
2026-01-24 14:39:13 -0500

    driving the hood logic

--- src/main/java/frc/robot/subsystems/hood/HoodIOBeta.java
+++ src/main/java/frc/robot/subsystems/hood/HoodIOBeta.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.hood;
 
+import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.Robot;
@@ -9,6 +10,7 @@ import frc.robot.util.sim.PhysicsSim;
 public class HoodIOBeta implements HoodIO {
     private final TalonFX hoodMotor;
     private final CANcoder hoodCANcoder;
+    private final PositionDutyCycle positionRequest = new PositionDutyCycle(0.0);
 
     public HoodIOBeta() {
         hoodMotor = new TalonFX(HoodConfigs.HOOD_MOTOR_ID, Constants.rioBus);
@@ -26,5 +28,10 @@ public class HoodIOBeta implements HoodIO {
     public void updateInputs(HoodIOInputs inputs) {
         inputs.hoodPosition = hoodMotor.getPosition().getValueAsDouble();
         inputs.hoodVelocity = hoodMotor.getVelocity().getValueAsDouble();
+    }
+
+    @Override
+    public void moveToPosition(double position) {
+        hoodMotor.setControl(positionRequest.withPosition(position));
     }
 }
