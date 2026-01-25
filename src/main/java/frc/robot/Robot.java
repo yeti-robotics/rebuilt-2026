f621489cad1288c2089cc207462e605d1e8a09f9
Prathik Saduneni <151557515+prthik@users.noreply.github.com>
2026-01-25 15:35:32 -0500

    sim field reset in the sim init method

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -146,7 +146,9 @@ public class Robot extends LoggedRobot {
 
     /** This function is called once when the robot is first started up. */
     @Override
-    public void simulationInit() {}
+    public void simulationInit() {
+        robotContainer.resetSimulationField();
+    }
 
     /** This function is called periodically whilst in simulation. */
     @Override
