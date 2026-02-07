a81ced30bdaf70982ccc7417ac9b589c81230cbb
yetirobotics <yetiroboticsengineering@gmail.com>
2026-02-07 15:54:38 -0500

    me when i mess up auto commands import

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,7 +24,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.AutoAimCommands;
-import frc.robot.commands.AutoCommands;
+import frc.robot.AutoCommands;
 import frc.robot.constants.Constants;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.climber.*;
