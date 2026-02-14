172cf83f2679c3ce2c022f6a0a4a992237c64cf0
John Doe <johndoe@email.com>
2026-02-14 15:50:00 -0500

    waitForSeconds(seconds) -> stack overflow

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeConfigsBeta.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.intake;
+
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+
+public class IntakeConfigsBeta {
+    static final int PRIMARY_INTAKE_MOTOR_ID = 12;
+    static final int SECONDARY_INTAKE_MOTOR_ID = 13;
+    static final double INTAKE_VOLTAGE = 1;
+    static final double OUTTAKE_VOLTAGE = -1;
+
+    static TalonFXConfiguration PRIMARY_TALONFX_CONFIGS = new TalonFXConfiguration()
+            .withMotorOutput(new MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive));
+
+    static TalonFXConfiguration SECONDARY_TALONFX_CONFIGS = new TalonFXConfiguration()
+            .withMotorOutput(new MotorOutputConfigs().withInverted(InvertedValue.CounterClockwise_Positive));
+}
