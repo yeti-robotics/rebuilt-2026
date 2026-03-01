124bb662c83b3a05282f16be726230aae6e3a4d3
Taran Duba <taran.duba3@gmail.com>
2026-02-28 21:11:52 -0500

    updated alpha hardware for linslide

--- src/main/java/frc/robot/subsystems/linslide/LinSlideIOReal.java
+++ src/main/java/frc/robot/subsystems/linslide/LinSlideIOReal.java
@@ -5,16 +5,19 @@ import static frc.robot.constants.Constants.rioBus;
 import static frc.robot.subsystems.linslide.LinSlideConfigsAlpha.*;
 
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import frc.robot.Robot;
 import frc.robot.constants.Constants;
 import frc.robot.util.sim.PhysicsSim;
 
 public class LinSlideIOReal implements LinSlideIO {
-    public final TalonFX linSlideMotor;
+    private final TalonFX linSlideMotor;
+    private TalonFX secondaryLinSlideMotor;
     private CANcoder linSlideCANcoder;
     private final DutyCycleOut dutyRequest = new DutyCycleOut(0.0);
     private final VoltageOut voltageRequest = new VoltageOut(0.0);
@@ -24,10 +27,12 @@ public class LinSlideIOReal implements LinSlideIO {
     public LinSlideIOReal() {
         if (currentMode == Constants.Mode.ALPHA) {
             linSlideMotor = new TalonFX(LinSlideConfigsAlpha.LIN_SLIDE_MOTOR_ID, rioBus);
+            secondaryLinSlideMotor = new TalonFX(LinSlideConfigsAlpha.OTHER_LIN_SLIDE_MOTOR_ID, rioBus);
             if (Robot.isSimulation()) {
                 PhysicsSim.getInstance().addTalonFX(linSlideMotor);
             }
             linSlideMotor.getConfigurator().apply(LinSlideConfigsAlpha.linSlideTalonFXConfigs);
+            secondaryLinSlideMotor.setControl(new Follower(LIN_SLIDE_MOTOR_ID, MotorAlignmentValue.Opposed));
             zeroPosition();
 
         } else {
