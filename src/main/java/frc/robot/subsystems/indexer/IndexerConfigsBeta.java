db9b4a56796d75a2939a9f8d6c6aa118ead8fdf7
yetirobotics <yetiroboticsengineering@gmail.com>
2026-03-08 12:35:44 -0400

    Update at 'Sun Mar 08 12:35:44 EDT 2026'

--- src/main/java/frc/robot/subsystems/indexer/IndexerConfigsBeta.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConfigsBeta.java
@@ -6,7 +6,7 @@ import com.ctre.phoenix6.signals.UpdateModeValue;
 public class IndexerConfigsBeta {
     static final int INDEXER_MOTOR_ID = 50;
     static final int INDEXER_CANRANGE_ID = 51;
-    public static final double TEST_INDEXER_SPEED = -1;
+    public static final double TEST_INDEXER_SPEED = -0.6;
 
     static final MotionMagicConfigs MOTION_MAGIC_CONFIGS =
             new MotionMagicConfigs().withMotionMagicCruiseVelocity(1).withMotionMagicAcceleration(2);
