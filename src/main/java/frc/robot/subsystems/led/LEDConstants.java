68c28647e4ce287b2f993716626fe0a54bd4d9c8
Taran Duba <taran.duba3@gmail.com>
2026-03-14 12:20:10 -0400

    led fixes pt. 2

--- src/main/java/frc/robot/subsystems/led/LEDConstants.java
+++ src/main/java/frc/robot/subsystems/led/LEDConstants.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.util.Color;
 
 public final class LEDConstants {
     public static final int LED_STRIP_PORT = 0;
-    public static final int LED_COUNT = 18;
+    public static final int LED_COUNT = 36;
     public static final Distance LED_SPACING = Meters.of(0.01);
     public static final Color YETI_BLUE = new Color(84, 229, 182);
     public static final Color VIHAAN_RED = new Color(197, 51, 34);
