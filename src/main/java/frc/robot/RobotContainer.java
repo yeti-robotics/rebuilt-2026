aa6e72c243801877aaf43c23dcdb10d686fbb49a
Taran Duba <taran.duba3@gmail.com>
2026-03-26 18:43:36 -0400

    added drive scaling based on battery voltage

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+import static frc.robot.constants.Constants.VOLTAGE_FILTER_CONSTANT;
 import static frc.robot.constants.Constants.currentMode;
 import static frc.robot.constants.FieldConstants.Hub.centerHubOpening;
 import static frc.robot.subsystems.indexer.IndexerConfigsBeta.TEST_INDEXER_SPEED;
@@ -16,10 +17,7 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.*;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -243,6 +241,20 @@ public class RobotContainer {
         Logger.recordOutput("Front Cam Transform", frontCameraPose);
     }
 
+    private double filteredVoltage = 12.0;
+    private double getFilteredVoltage() {
+        double raw = RobotController.getBatteryVoltage();
+        filteredVoltage = VOLTAGE_FILTER_CONSTANT * raw + (1 - VOLTAGE_FILTER_CONSTANT) * filteredVoltage;
+        return filteredVoltage;
+    }
+
+    private double getScalarDriveSpeed() {
+        double voltage = getFilteredVoltage();
+        if (voltage >= 12.0) return 1.0;
+        if (voltage < 10.5) return 0.6;
+        return 0.6 + (voltage - 10.5) / (12.0 - 10.5) * (0.4);
+    }
+
     /**
      * Use this method to define your button->command mappings. Buttons can be created by
      * instantiating a {@link GenericHID} or one of its subclasses ({@link
@@ -251,9 +263,9 @@ public class RobotContainer {
      */
     private void configureRealBindings() {
         drive.setDefaultCommand(drive.applyRequest(() -> driveRequest
-                .withVelocityX(-controller.getLeftY() * climbState.kSpeedAt12Volts.magnitude())
-                .withVelocityY(-controller.getLeftX() * climbState.kSpeedAt12Volts.magnitude())
-                .withRotationalRate(-controller.getRightX() * climbState.MaFxAngularRate)));
+                .withVelocityX(-controller.getLeftY() * climbState.kSpeedAt12Volts.magnitude() * getScalarDriveSpeed())
+                .withVelocityY(-controller.getLeftX() * climbState.kSpeedAt12Volts.magnitude() * getScalarDriveSpeed())
+                .withRotationalRate(-controller.getRightX() * climbState.MaFxAngularRate * getScalarDriveSpeed())));
 
         controller
                 .povUp()
@@ -406,6 +418,9 @@ public class RobotContainer {
 
         Logger.recordOutput("AutoAimCommands/Shuttle Map/ideal shuttle distance", shuttleDistance);
         Logger.recordOutput("Drive/Swerve Lock State", swerveLockState);
+
+        Logger.recordOutput("Drive/Scalar Drive Speed", getScalarDriveSpeed());
+        Logger.recordOutput("Drive/Filtered Voltage", getFilteredVoltage());
     }
 
     public void saveLog() {
