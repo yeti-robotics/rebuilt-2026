e422d0667396c1a8cd0f622b280732b35d5a33b1
Taran Duba <taran.duba3@gmail.com>
2026-04-11 23:29:34 -0400

    added shooter default cmd

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -23,6 +23,7 @@ public class Shooter extends SubsystemBase {
 
     public Shooter(ShooterIO io) {
         this.io = io;
+        setDefaultCommand(shoot(2)); // TODO: TEST IN PHOENIX TUNER X!
     }
 
     public AngularVelocity getVelocity() {
