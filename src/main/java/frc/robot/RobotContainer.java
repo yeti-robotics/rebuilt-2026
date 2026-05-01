443a8903a06705badaf0e1888e8f5ce3caf7a38c
yetirobotics <yetiroboticsengineering@gmail.com>
2026-05-01 09:15:49 -0500

    Update at 'Fri May 01 09:15:49 CDT 2026'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -246,7 +246,7 @@ public class RobotContainer {
 
         controller.start().onTrue(Commands.runOnce(drive::seedFieldCentric, drive));
 
-        controller.x().whileTrue(linSlide.applyPower(1));
+        controller.x().whileTrue(linSlide.applyPower(0.5));
         controller
                 .b()
                 .whileTrue(linSlide.applyPower(-LinSlideConfigsBeta.DEPLOY_SPEED)
