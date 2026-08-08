6deb033af2228501f8853a77c4cfe636809c019e
Fionn <fionn.gaioni@gmail.com>
2026-08-08 15:59:20 -0400

    no-swivel cam transforms

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -51,15 +51,15 @@ public class VisionConstants {
     public static double linearStdDevMegatag2Factor = 0.5; // More stable than full 3D solve
     public static double angularStdDevMegatag2Factor = Double.POSITIVE_INFINITY; // No rotation data available
 
-    //yellow cone = turret cam
+    // yellow cone = turret cam
     public static Transform3d frontCamTrans = new Transform3d(
-            new Translation3d(0.24, -0.42, 0.47), new Rotation3d(0, Math.toRadians(-20), Math.toRadians(270)));
+            new Translation3d(.35, -0.42, 0.47), new Rotation3d(0, Math.toRadians(-22.5), Math.toRadians(315)));
 
-    //green = left cam
+    // green = left cam
     public static Transform3d leftCamTrans = new Transform3d(
-            new Translation3d(0.3429, .35, 0.5715), new Rotation3d(0, Math.toRadians(-15), Math.toRadians(45)));
+            new Translation3d(.35, -.1, .47), new Rotation3d(0, Math.toRadians(-22.5), Math.toRadians(45)));
 
-    //red = forward cam
+    // red = forward cam
     public static Transform3d rightCamTrans = new Transform3d(
             new Translation3d(.2, 0.2, 0.4445),
             new Rotation3d(Math.toRadians(180), Math.toRadians(-15), Math.toRadians(180)));
